71572e16f46f67fa0c62e0b1c6ac9cfa2b20af54
seairs <sea.airs77@gmail.com>
2022-05-16 23:27:39 -0400

    event belongs to a single creator, a creator is of class User

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+    #has_many :post_authorings, foreign_key: :authored_post_id
+    #has_many :authors, through: :post_authorings, source: :post_author
+    belongs_to 	:creator, :class_name => "User"
+    #belongs_to :creator, class_name: "User"
 end
