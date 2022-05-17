0d2b50f42d64670ee6b487bfdad4a89b9a484112
seairs <sea.airs77@gmail.com>
2022-05-17 01:50:30 -0400

    adding in scope for selecting past/future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,12 @@ class Event < ApplicationRecord
 
     # The Event belongs to a creator which is of class User
     belongs_to 	:creator, :class_name => "User"
-    # The Event has many attendees through which it related in event_attendees table
-    has_many :attendees, :through => :event_attendees
     # The Event has many event_attendees lists, each event that is attended has an id
     has_many :event_attendees,  :foreign_key => :attended_event_id
+    # The Event has many attendees through which it related in event_attendees table
+    has_many :attendees, :through => :event_attendees
+
+#scope :past, 	 -> { where("Date <  ?", Date.today).order('Date DESC') }
+
+
 end
