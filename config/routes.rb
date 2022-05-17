e1385f4950ecef6256199c3543965435bcdd8e57
seairs <sea.airs77@gmail.com>
2022-05-17 01:52:44 -0400

    added routes for event_attendees

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'user/registrations' }
   resources :events
   resources :users, only: [:show]
+  resources :event_attendees, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
