a607edce7ed28b864f1c8bba9811036a3caeeee4
seairs <sea.airs77@gmail.com>
2022-05-17 00:00:01 -0400

    add belongs_to to through table

--- app/models/event_attendee.rb
+++ app/models/event_attendee.rb
@@ -1,2 +1,12 @@
 class EventAttendee < ApplicationRecord
+# Belongs to an attendee    
+belongs_to :attendee, :class_name => "User"
+# Belongs to an attended event
+belongs_to :attended_event, :class_name => "Event"
+
+# Has an attendee_id
+validates :attendee_id, presence: true
+# Has an attended_event_id
+validates :attended_event_id, presence: true
+
 end
