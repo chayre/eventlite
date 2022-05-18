b2b298f0ca9f8eae968e482a3c5eed32e98f790b
seairs <sea.airs77@gmail.com>
2022-05-18 19:56:05 -0400

    if user deletes their account, deletes their hosted events

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # The User has_many events they can be the creator for; they are referenced by a creator_id
-  has_many :events, :foreign_key => :creator_id
+  has_many :events, :foreign_key => :creator_id, dependent: :delete_all
   # The User has_many event_attendees tables; in which they are referenced by the attendee_id
   has_many :event_attendees,  :foreign_key => :attendee_id
   # The User has_many attended_events; through the event_attendees list
