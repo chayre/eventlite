e812bf556434469112a3465e247005fd9bfc00fe
seairs <sea.airs77@gmail.com>
2022-05-18 18:57:37 -0400

    added admin column to user (default: false)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_225330) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_225640) do
   create_table "creators_events", id: false, force: :cascade do |t|
     t.integer "event_id", null: false
     t.integer "creator_id", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_225330) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
