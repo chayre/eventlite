733b56e2b205636641fe6f9846df9186a4eedf58
seairs <sea.airs77@gmail.com>
2022-05-18 19:57:45 -0400

    populate db with admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+  {name: 'Admin', username: 'Admin', email: "admin@eventlite.com", password: "Admin1", password_confirmation: "Admin1", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: true}
+  ])
