b33a5911d57ece85530956d2ff205ad79e73ac32
seairs <sea.airs77@gmail.com>
2022-05-18 17:08:47 -0400

    add ability to edit user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'user/registrations' }
   resources :events
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   resources :event_attendees, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
