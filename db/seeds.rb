5602f10fdd2cea33da0d3180dd46c51aa7e232e5
seairs <sea.airs77@gmail.com>
2022-05-18 20:31:52 -0400

    populated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,9 @@
 # Examples:
 #
 User.create!([
-  {name: 'Admin', username: 'Admin', email: "admin@eventlite.com", password: "Admin1", password_confirmation: "Admin1", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: true}
-  ])
+  {name: 'Admin', username: 'Admin', email: "admin@eventlite.com", password: "Admin1", password_confirmation: "Admin1", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: true},
+  {name: 'John Cena', username: 'john93', email: "john@gmail.com", password: "password", password_confirmation: "password", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: false},
+  {name: 'Ricky Bobby', username: 'rickybobby', email: "ricky@gmail.com", password: "password", password_confirmation: "password", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: false},
+  {name: 'Hal Ford', username: 'hal12', email: "hal@gmail.com", password: "password", password_confirmation: "password", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: false},
+  {name: 'Sarah Ee', username: 'sarahee', email: "sarah@gmail.com", password: "password", password_confirmation: "password", reset_password_token: nil, remember_created_at: nil, created_at: "2022-02-06 14:02:10", updated_at: nil, admin: false}
+])
